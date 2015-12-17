542a22bbb8bb27ebdb742f8dec314d6d713efe31
byplayer <byplayer100@gmail.com>
2015-12-17 14:14:32 +0800

    add cleanup batch

---
+++ clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r .deps
+rm Makefile
+rm Makefile.in
+rm aclocal.m4
+rm -r autom4te.cache
+rm autoscan.log
+rm compile
+rm config.h
+rm config.h.in
+rm config.h.in~
+rm config.log
+rm config.status
+rm configure
+rm depcomp
+rm install-sh
+rm missing
+rm setlayout.o
+rm stamp-h1
